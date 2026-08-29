64950f751cf4caa9fc0d6f4d25a6dafa69e5faa0
prabhatkrmishra <mprabhat774@gmail.com>
2026-08-29 17:10:40 +0530

    Fix login cookie settings for HTTP and add debug logging for DB configs
    - Make server.servlet.session.cookie.secure and same-site configurable
      via SERVER_COOKIE_SECURE and SERVER_COOKIE_SAME_SITE env vars
      (default true/strict for production, set to false/lax for HTTP dev)
    - Add debug logging to PostgresConfig and MysqlConfig to diagnose
      conditional loading issues

--- src/main/java/com/pkmprojects/mongodbserver/config/MysqlConfig.java
+++ src/main/java/com/pkmprojects/mongodbserver/config/MysqlConfig.java
@@ -1,5 +1,7 @@
 package com.pkmprojects.mongodbserver.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +17,13 @@ import org.springframework.beans.factory.annotation.Qualifier;
 @ConditionalOnProperty(name = "app.mysql.enabled", havingValue = "true")
 public class MysqlConfig {
 
+    private static final Logger log = LoggerFactory.getLogger(MysqlConfig.class);
+
     @Bean
     DataSource mysqlDataSource(
             @Value("${app.mysql.uri:jdbc:mysql://127.0.0.1:9816/mysql?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC}") String uri,
             @Value("${MYSQL_ROOT_PASSWORD:root}") String password) {
+        log.info("MysqlConfig: Creating mysqlDataSource with uri={}", uri);
         DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
         ds.setUrl(uri);
@@ -30,6 +35,7 @@ public class MysqlConfig {
 
     @Bean
     JdbcTemplate mysqlJdbcTemplate(@Qualifier("mysqlDataSource") DataSource mysqlDataSource) {
+        log.info("MysqlConfig: Creating mysqlJdbcTemplate");
         return new JdbcTemplate(mysqlDataSource);
     }
 }
--- src/main/java/com/pkmprojects/mongodbserver/config/PostgresConfig.java
+++ src/main/java/com/pkmprojects/mongodbserver/config/PostgresConfig.java
@@ -1,5 +1,7 @@
 package com.pkmprojects.mongodbserver.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +17,14 @@ import org.springframework.beans.factory.annotation.Qualifier;
 @ConditionalOnProperty(name = "app.postgres.enabled", havingValue = "true")
 public class PostgresConfig {
 
+    private static final Logger log = LoggerFactory.getLogger(PostgresConfig.class);
+
     @Bean
     DataSource postgresDataSource(
             @Value("${app.postgres.uri:jdbc:postgresql://127.0.0.1:9813/postgres}") String uri,
             @Value("${POSTGRES_ROOT_USER:root}") String username,
             @Value("${POSTGRES_ROOT_PASSWORD:root}") String password) {
+        log.info("PostgresConfig: Creating postgresDataSource with uri={}, username={}", uri, username);
         DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName("org.postgresql.Driver");
         ds.setUrl(uri);
@@ -30,6 +35,7 @@ public class PostgresConfig {
 
     @Bean
     JdbcTemplate postgresJdbcTemplate(@Qualifier("postgresDataSource") DataSource postgresDataSource) {
+        log.info("PostgresConfig: Creating postgresJdbcTemplate");
         return new JdbcTemplate(postgresDataSource);
     }
 }
