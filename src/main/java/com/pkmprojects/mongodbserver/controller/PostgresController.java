4b7ced3a8b1a2bfbf048fec69bfe49c548ff2200
prabhatkrmishra <mprabhat774@gmail.com>
2026-08-29 16:26:41 +0530

    Add @ConditionalOnProperty to PostgresController and MysqlController

--- src/main/java/com/pkmprojects/mongodbserver/controller/PostgresController.java
+++ src/main/java/com/pkmprojects/mongodbserver/controller/PostgresController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,6 +36,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @Controller
+@ConditionalOnProperty(name = "app.postgres.enabled", havingValue = "true")
 @RequestMapping("/postgres")
 public class PostgresController {
 
